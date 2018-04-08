a115857ed64288a5eedce377e79710c5a91767a1
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-04-07 21:54:19 -0500

    Cambios de reto

---
+++ app/controllers/artist_controller.rb
@@ -0,0 +1,10 @@
+class ArtistController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+    @album = @artist.albums
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/artists', to: 'artist#index'
+
+  get 'artists/:id', to: 'artist#show', as: 'show_artists'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
