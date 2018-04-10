335be2e80bfe35a8b661d7c2c4efa4626836ab23
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-04-09 19:40:14 -0500

    Cambios

--- test/controllers/artists_controller_test.rb
+++ test/controllers/artists_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
-class ArtistControllerTest < ActionDispatch::IntegrationTest
+class ArtistsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get artist_index_url
+    get artists_index_url
     assert_response :success
   end
 
   test "should get show" do
-    get artist_show_url
+    get artists_show_url
     assert_response :success
   end
 
