efc80290be242e62e55cf0276a14184d1fe7a89a
Leonel Oliveros <leoneloliveros.co@gmail.com>
2018-04-09 19:29:46 -0500

    Cambios

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/artists', to: 'artist#index', as: 'artists'
+  get '/artists', to: 'artists#index', as: 'artists'
 
-  get 'artists/:id', to: 'artist#show', as: 'show_artists'
+  get 'artists/:id', to: 'artists#show', as: 'show_artists'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
